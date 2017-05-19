070ec9718a0ee82dd2b87d7cdc95bed9ff30d929
Pentatonic <john.pentatonic@gmail.com>
2017-05-19 08:44:37 +0800

    Add "units.list".
    1. Add "units.list" parsing.
    2. Remember changing to your credentials for vision API.

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 // Imports the Google Cloud client library
 const Vision = require('@google-cloud/vision');
+const fs = require('fs');
+const lineReader = require('line-reader');
 
 // Instantiates a client
-const vision = Vision(
+const credentials = [
   {
     projectId: 'react-google-maps',
     keyFilename: 'react-google-maps-476def2de5c2.json',
-  }
-);
+  },
+  {
+    projectId: 'nimble-answer-165506',
+    keyFilename: 'Tutorial Project-e98b0d21b706.json',
+  },
+];
 
-// The path to the local image file, e.g. "/path/to/image.png"
-const fileName = 'input.jpg';
+const vision = Vision(
+  credentials[0]
+  // credentials[1]
+);
 
 const options = {
   imageContext:
@@ -20,15 +28,33 @@ const options = {
     ]
   }
 };
-// Performs text detection on the local file
-vision.detectText(fileName, options)
-  .then((results) => {
-    const detections = results[0];
 
-    console.log('Text:');
-    detections.forEach((text) => console.log(text));
-    // console.log(JSON.stringify(detections));
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
+// open OCR list file
+var OCRList = "units.list";
+var encode = "utf8";
+
+lineReader.eachLine(OCRList, encode, function (imgFullname, last) {
+  var regex = /([^\/]+)$/;
+  var match = imgFullname.match(regex);
+  var filename = match[0];
+  console.log(imgFullname);
+
+  var outputName = "./OCRText/" + filename + ".txt";
+  console.log("Recognition to " + outputName);
+
+  // Performs text detection on the local file
+  vision.detectText(imgFullname, options)
+    .then((results) => {
+      const detections = results[0];
+      //detections.forEach((text) => console.log(text));
+
+      fs.writeFile(outputName, detections, function (err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    })
+    .catch((err) => {
+      console.error('ERROR:', err);
+    });
+});
