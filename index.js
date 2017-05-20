14f4b7d0069923ea5d2f678ff4e6c66cc0fe60a7
Pentatonic <john.pentatonic@gmail.com>
2017-05-20 13:48:30 +0800

    Resize intput image width to 2048 if size > 4MB.
    Open console as Administrator:
    1. npm install --global --production windows-build-tools
    2. npm install

--- index.js
+++ index.js
@@ -37,65 +37,94 @@ const inputDir = 'input_images/';
 const outputDir = 'output/';
 
 function google_vision_api(imgFullname, vision_callback) {
-  async.waterfall([
-    // Check file existence
-    (callback) => {
-      var regex = /([^\/]+)$/;
-      var match = imgFullname.match(regex);
-      var filename = match[0];
-      const inputFilename = inputDir + filename;
-      const outputFilename = outputDir + filename + '.json';
+  var regex = /([^\/]+)$/;
+  var match = imgFullname.match(regex);
+  var filename = match[0];
+  const outputFilename = outputDir + filename + '.json';
 
-      if (fs.existsSync(outputFilename)) {
-        const err = '!!! Abort, file already exist ' + outputFilename;
-        callback(err);
-      }
-      else {
-        callback(null, inputFilename, outputFilename);
-      }
-    },
-    // Do text detection
-    function (inputFilename, outputFilename, callback) {
-      console.log('<-- Detecting text on: ' + inputFilename);
-
-      vision.detectText(inputFilename, options)
-        .then((results) => {
-          const detections = results[0];
-          //detections.forEach((text) => console.log(text));
-          //console.log(JSON.stringify(results));
-          callback(null, results, outputFilename);
-        })
-        .catch((err) => {
-          console.error('!!! ' + '\nVision API ERROR:', err);
-          callback(err);
-        })
-    },
-
-    // Write detection result to file
-    (results, outputFilename, callback) => {
-      console.log('--> Write result to: ' + outputFilename);
-
-      fs.writeFile(outputFilename, JSON.stringify(results), function (err) {
-        if (err) {
+  async.waterfall(
+    // tasks
+    [
+      // Check file existence
+      (callback) => {
+        if (fs.existsSync(outputFilename)) {
+          const err = '!!! Abort, file already exist ' + outputFilename;
           callback(err);
         }
         else {
-          callback(null, 'done');
+          callback(null);
         }
-      });
-    }
-  ],
+      },
 
+      // resize those files > 4MB
+      (callback) => {
+        var inputFilename = inputDir + filename;
+        const stats = fs.statSync(inputFilename);
+        const fileSizeInMB = stats.size / 1024 / 1024;
+        //console.log('Check file size: ' + inputFilename);
+
+        if (fileSizeInMB > 3.9) {
+          var sharp = require('sharp');
+          const newInputFilename = inputDir + '_' + filename;
+          sharp(inputFilename)
+            .resize(2048)
+            .toFile(newInputFilename, (err, info) => {
+              if (err) {
+                callback(err);
+              }
+              else {
+                callback(null, newInputFilename);
+              }
+            });
+        }
+        else {
+          callback(null, inputFilename);
+        }
+      },
+
+      // Do text detection
+      (inputFilename, callback) => {
+        console.log('<-- Detecting text on: ' + inputFilename);
+
+        vision.detectText(inputFilename, options)
+          .then((results) => {
+            const detections = results[0];
+            //detections.forEach((text) => console.log(text));
+            callback(null, results);
+          })
+          .catch((err) => {
+            console.error('!!! ' + '\nVision API ERROR:', err);
+            callback(err);
+          })
+      },
+
+      // Write detection result to file
+      (results, callback) => {
+        console.log('--> Write result to: ' + outputFilename);
+
+        fs.writeFile(outputFilename, JSON.stringify(results), function (err) {
+          if (err) {
+            callback(err);
+          }
+          else {
+            callback(null, 'done');
+          }
+        });
+      }
+    ],
+
+    // if the above try/catch catches something, we will end up here
+    // otherwise we will receive 'done' as the value of status
+    // after the third function has finished
     (err, status) => {
-      // if the above try/catche catches something, we will end up here
-      // otherwise we will receive 'done' as the value of status
-      // after the third function has finished
+
       if (err) {
         console.log(err);
       }
       else {
-      console.log('status: ' + status);
+        console.log('status: ' + status);
       }
+
       vision_callback();
     }
   );
